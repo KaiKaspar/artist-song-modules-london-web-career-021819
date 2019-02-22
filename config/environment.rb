d251aa83fae87c3fb3771ea539aecd4ecfdf3be1
github username <github email address>
2019-02-22 18:06:34 +0000

    Done.

--- config/environment.rb
+++ config/environment.rb
@@ -1,8 +1,8 @@
 require 'pry'
 
-# require_relative '../lib/concerns/findable'
-# require_relative '../lib/concerns/memorable'
-# require_relative '../lib/concerns/paramable'
+require_relative '../lib/concers/findable'
+require_relative '../lib/concers/memorable'
+require_relative '../lib/concers/paramable'
 
 require_relative '../lib/artist.rb'
 require_relative '../lib/song.rb'
